35dc79a4d51d7a988fdc98c6c8b8fb0b32b2c543
dylanberry <dylan@dylanberry.com>
2021-02-11 17:08:35 -0500

    Initial spike

---
+++ scripts/Set-CanvasAppOwnerAndSharing.ps1
@@ -0,0 +1,36 @@
+param(  
+  [Parameter(Mandatory=$true)]
+  [string]$EnvironmentName,
+  
+  [Parameter(Mandatory=$true)]
+  [string]$OwnerAccountEmail,
+  
+  [Parameter]
+  [string]$ViewerGroup
+)
+
+If(-not(Get-InstalledModule Microsoft.PowerApps.Administration.PowerShell -ErrorAction silentlycontinue)) {
+  Set-PSRepository PSGallery -InstallationPolicy Trusted
+  Install-Module Microsoft.PowerApps.Administration.PowerShell -Confirm:$False -Force -AllowClobber -MaximumVersion 2.0.102
+}
+Import-Module Microsoft.PowerApps.Administration.PowerShell
+
+Add-PowerAppsAccount
+
+$environment = Get-AdminPowerAppEnvironment | ? {$_.DisplayName -eq $EnvironmentName}
+
+$canvasApps = Get-AdminPowerApp -EnvironmentName $environment.EnvironmentName | ? {$_.Owner | Select | ? type -eq "ServicePrincipal"} | Select *
+$ownerAccount = Get-UsersOrGroupsFromGraph -SearchString $OwnerAccountEmail | ? {$_.UserPrincipalName -eq $OwnerAccountEmail}
+
+if ($ViewerGroup) {
+  $groupAccount = Get-UsersOrGroupsFromGraph -SearchString $ViewerGroup | ? {$_.DisplayName -eq $ViewerGroup}
+}
+
+Foreach ($canvasApp in $canvasApps) {
+    Set-AdminPowerAppOwner -AppName $canvasApp.AppName -EnvironmentName $environment.EnvironmentName -AppOwner $ownerAccount.ObjectId
+    Set-AdminPowerAppRoleAssignment -AppName $canvasApp.AppName -EnvironmentName $environment.EnvironmentName -RoleName "CanEdit" -PrincipalType "User" -PrincipalObjectId $ownerAccount.ObjectId -Verbose
+    
+    if ($ViewerGroup) {
+      Set-AdminPowerAppRoleAssignment -AppName $canvasApp.AppName -EnvironmentName $environment.EnvironmentName -RoleName "CanView" -PrincipalType "Group" -PrincipalObjectId $groupAccount.Objectd -Verbose
+    }
+}
